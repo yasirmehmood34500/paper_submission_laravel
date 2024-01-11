9fa03addf16f1c6dcdcf52215e9a212db705fe49
YasirMehmood <apple@192.168.0.100>
2024-01-11 21:01:17 +0500

    login as in paper detail page set

--- resources/views/pages/paper/detail.blade.php
+++ resources/views/pages/paper/detail.blade.php
@@ -17,10 +17,10 @@
                     <button class="btn btn-primary assing_review_btn">Assign to Review</button>
                 @endif
                 @can('login_as_user')
-                    {{-- @if ($paper->user_id != auth()->id()) --}}
-                    <a href="{{ route('login_as_user_link', ['user_id' => $paper->user_id]) }}" class="btn btn-info">Login
-                        As</a>
-                    {{-- @endif --}}
+                    @if ($paper->user_id != auth()->id())
+                        <a href="{{ route('login_as_user_link', ['user_id' => $paper->user_id]) }}" class="btn btn-info">Login
+                            As</a>
+                    @endif
                 @endcan
             </div>
         </div>
