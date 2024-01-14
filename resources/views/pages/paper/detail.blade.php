dd42fe067a3cb601ba28068636e549708ebe9ddc
YasirMehmood <apple@192.168.0.102>
2024-01-14 12:58:28 +0500

    remove pre tag from comment

--- resources/views/pages/paper/detail.blade.php
+++ resources/views/pages/paper/detail.blade.php
@@ -55,9 +55,7 @@
                 <p>{{ $paper->keywords }}</p>
                 @if (Gate::check('view_all_submission') || $paper->user_id == auth()->id())
                     <b>Comment for Editor:</b>
-                    <p>
-                        <pre>{{ $paper->comment_for_editor }}</pre>
-                    </p>
+                    <p>{{ $paper->comment_for_editor }}</p>
                 @endif
                 <div class="pull-right">
                     @can('login_as_user')
@@ -124,9 +122,7 @@
                         @foreach ($admin_decisions as $admin_decision)
                             <tr>
                                 <td>{{ $admin_decision->review_type->name }}</td>
-                                <td>
-                                    <pre>{{ $admin_decision->comment }}</pre>
-                                </td>
+                                <td>{{ $admin_decision->comment }}</td>
                                 <td>
                                     @if ($admin_decision->file != '')
                                         @foreach (explode(',', $admin_decision->file) as $single_file)
